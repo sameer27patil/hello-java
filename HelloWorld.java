f088bfb5200222f7f1aeee8b8d745be2854557b3
sameer27patil <71928005+sameer27patil@users.noreply.github.com>
2024-07-05 16:28:16 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("This is Sameer Patil");
+    System.out.println("Hello, HCLtech");
+    System.out.println("This is Aarya Patil");
   }
 }
